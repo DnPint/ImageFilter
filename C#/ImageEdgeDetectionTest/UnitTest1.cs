7dd26b4638cb31c85e4c718b603b0633569ba369
DnPint <daniel.pintodem@hes-so.ch>
2022-11-14 14:26:10 +0100

    added method names

---
+++ C#/ImageEdgeDetectionTest/UnitTest1.cs
@@ -0,0 +1,75 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ImageEdgeDetectionTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestNightFilter()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestMiamiFilter()
+        {
+
+        }
+
+        [TestMethod]
+        public void XKirsch3x3Vertical_YKirsch3x3Vertical()
+        {
+
+        }
+
+        [TestMethod]
+        public void XKirsch3x3Vertical_YKirsch3x3Horizontal()
+        {
+
+        }
+
+        [TestMethod]
+        public void XKirsch3x3Vertical_YPrewitt3x3Vertical()
+        {
+
+        }
+
+        [TestMethod]
+        public void XKirsch3x3Horizontal_YKirsch3x3Vertical()
+        {
+
+        }
+
+        [TestMethod]
+        public void XKirsch3x3Horizontal_Ykirsch3x3Horizontal()
+        {
+
+        }
+
+        [TestMethod]
+        public void XKirsch3x3Horizontal_YPrewitt3x3Vertical()
+        {
+
+        }
+
+        [TestMethod]
+        public void XPrewitt3x3Vertical_YKirsch3x3Vertical()
+        {
+
+        }
+
+        [TestMethod]
+        public void XPrewitt3x3Vertical_YKirsch3x3Horizontal()
+        {
+
+        }
+
+        [TestMethod]
+        public void XPrewitt3x3Vertical_YPrewitt3x3Vertical()
+        {
+
+        }
+    }
+}
